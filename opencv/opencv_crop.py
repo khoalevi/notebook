44cb15e9e2c0328d0b64db1d5f6b886e3415c2a8
Khoa Vo <khoa.hyper@gmail.com>
2021-09-09 09:36:32 +0700

    add opencv crop

---
+++ opencv/opencv_crop.py
@@ -0,0 +1,15 @@
+import argparse
+import imutils
+import cv2
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--image", type=str, default="cat.jpg", help="path to input image")
+args = vars(parser.parse_args())
+
+image = cv2.imread(args["image"])
+cv2.imshow("Original", image)
+
+face = image[80:425, 120:475]
+cv2.imshow("Face", face)
+
+cv2.waitKey(0)
